4b8739df41ff4a0e88bd7ef98c5e9afaa2863547
capellinx <capellaaa7@gmail.com>
2024-12-05 12:11:25 -0300

    feat: create home page application

---
+++ src/app/home-page.tsx
@@ -0,0 +1,19 @@
+export function HomePage() {
+  return (
+    <main className="w-full flex flex-col items-center justify-center h-screen">
+      <div className="w-1/5 flex flex-col items-center justify-center gap-6 ">
+        <h1 className="text-3xl font-inter font-light">Olá, seja bem-vindo</h1>
+        <input
+          type="text"
+          placeholder="Digite seu nome:"
+          className="w-full bg-transparent border-2 border-gray rounded-sm p-2 focus:outline-orange-500"
+        />
+        <button
+          className="w-full p-2 rounded-sm text-white bg-orange-500 focus:outline-black hover:bg-orange-700"
+        >
+          Entrar
+        </button>
+      </div>
+    </main>
+  )
+}
