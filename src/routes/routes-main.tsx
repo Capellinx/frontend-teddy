393711f1f839960570adf8cd7e4387ed71801b7e
capellinx <capellaaa7@gmail.com>
2024-12-06 09:30:43 -0300

    feat: create new route to move home

--- src/routes/routes-main.tsx
+++ src/routes/routes-main.tsx
@@ -1,11 +1,13 @@
 import {Route, Routes} from 'react-router-dom';
-import {LoginPage} from '../app/login.tsx';
+import {HomePage} from '../app/home';
+import {LoginPage} from '../app/login';
 
 
 export const RoutesMain = () => {
   return (
     <Routes>
       <Route path="/" element={<LoginPage/>} />
+      <Route path="/home" element={<HomePage/>} />
       
       {/*<Route path="*" element={< />} />*/}
     </Routes>
