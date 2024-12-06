c475a78f5aae1e3f031796e049f83b7070043f12
capellinx <capellaaa7@gmail.com>
2024-12-06 17:58:41 -0300

    feat: create route private

--- src/routes/routes-main.tsx
+++ src/routes/routes-main.tsx
@@ -1,13 +1,17 @@
 import {Route, Routes} from 'react-router-dom';
 import {HomePage} from '../app/home';
 import {LoginPage} from '../app/login';
+import {AuthControllerAccess} from '../utils/auth-control-access.tsx';
 
 
 export const RoutesMain = () => {
   return (
     <Routes>
-      <Route path="/" element={<LoginPage/>} />
-      <Route path="/home" element={<HomePage/>} />
+      <Route path="/" element={<LoginPage/>}/>
+      
+      <Route element={<AuthControllerAccess/>}>
+        <Route path="/home" element={<HomePage/>}/>
+      </Route>
       
       {/*<Route path="*" element={< />} />*/}
     </Routes>
