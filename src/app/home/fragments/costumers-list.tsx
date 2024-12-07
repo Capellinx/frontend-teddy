e2f7c8a16d6831519fcb032a3e2d0b4641b57ef4
capellinx <capellaaa7@gmail.com>
2024-12-07 13:33:42 -0300

    feat: add state to control modal delete costumer

--- src/app/home/fragments/costumers-list.tsx
+++ src/app/home/fragments/costumers-list.tsx
@@ -1,6 +1,7 @@
 import {PencilIcon, PlusIcon, Trash2} from 'lucide-react';
 import {useState} from 'react';
 import {Costumer} from '../../../@types/costumer';
+import {DeleteCostumerForm} from '../../../components/forms/delete-costumer-form.tsx';
 import {UpdateCostumerForm} from '../../../components/forms/update-costumer-form.tsx';
 import {ModalCostumers} from '../../../components/modal/create-costumer-modal.tsx';
 import {costumersList} from '../index.tsx';
@@ -12,38 +13,48 @@ interface CostumersListProps {
 
 export function CostumersList({costumers}: CostumersListProps) {
   const [isOpen, setIsOpen] = useState(false);
-  const [updateCostumer, setNewUpdateCostumer] = useState<Costumer.Update>()
+  const [isOpenDelete, setIsOpenDelete] = useState(false);
   
-  function handleInviteCostumerEdit(costumer: Costumer.Update) {
+  const [updateCostumer, setNewUpdateCostumer] = useState<Costumer.Update>()
+  const [deleteCostumer, setDeleteCostumer] = useState<Costumer.Create>()
+  
+  function handleUpdateCostumer(costumer: Costumer.Update) {
     setIsOpen(!isOpen);
     setNewUpdateCostumer(costumer)
   }
   
+  function handleDeleteCostumer(costumer: Costumer.Create) {
+    setDeleteCostumer(costumer);
+    setIsOpenDelete(!isOpenDelete)
+  }
+  
   return (
     <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
-      {costumers.map((item) => (
+      {costumers.map((costumer) => (
         <li
-          key={item.id}
+          key={costumer.id}
           className="bg-[#ffff] text-center p-6 rounded-md mt-2.5 font-inter"
         >
           <div className="flex flex-col gap-2.5 text-[16px]">
             <p>
-              <strong>{item.name}</strong>
+              <strong>{costumer.name}</strong>
             </p>
-            <p>{item.salary}</p>
-            <p>{item.company}</p>
+            <p>{costumer.salary}</p>
+            <p>{costumer.company}</p>
           </div>
-          <div className="flex items-center justify-between mt-4">
+          <div className="flex costumers-center justify-between mt-4">
             <button>
               <PlusIcon/>
             </button>
             <button>
               <PencilIcon
-                onClick={() => handleInviteCostumerEdit(item)}
+                onClick={() => handleUpdateCostumer(costumer)}
               />
             </button>
             <button>
-              <Trash2 className="text-red" />
+              <Trash2
+                onClick={() => handleDeleteCostumer(costumer)}
+                className="text-red"/>
             </button>
           </div>
         </li>
@@ -58,6 +69,19 @@ export function CostumersList({costumers}: CostumersListProps) {
           />
         </ModalCostumers>
       )}
+      
+      {isOpenDelete && (
+        <ModalCostumers
+          title="Excluir cliente:"
+          setIsOpen={setIsOpenDelete}>
+          <DeleteCostumerForm
+            setIsOpenDelete={setIsOpenDelete}
+            costumer={deleteCostumer!}
+          />
+        </ModalCostumers>
+      )}
+    
+    
     </ul>
   )
 }
