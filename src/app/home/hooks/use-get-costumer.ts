de30a344492d6374e541ad1f034f2140eace61dc
capellinx <capellaaa7@gmail.com>
2024-12-08 13:10:51 -0300

    feat: implements filter of page

--- src/app/home/hooks/use-get-costumer.ts
+++ src/app/home/hooks/use-get-costumer.ts
@@ -2,16 +2,24 @@ import {useQuery} from '@tanstack/react-query';
 import {api} from '../../../service/api.ts';
 
 
-export const useGetCostumer = () => {
+interface UseGetCostumerProps {
+  page?: number;
+  limit?: number;
+}
+
+export const useGetCostumer = ({page = 1, limit = 12}: UseGetCostumerProps) => {
   const {data} = useQuery({
-    queryKey: ['costumer'],
-    queryFn: async () => handleGetCostumers(),
+    queryKey: ['costumer', page, limit],
+    queryFn: () => handleGetCostumers({
+      page,
+      limit,
+    }),
   })
   
   
-  async function handleGetCostumers() {
+  async function handleGetCostumers({page, limit}: UseGetCostumerProps) {
     try {
-      const {data} = await api.get('/costumer');
+      const {data} = await api.get(`/costumer?limit=${limit}&page=${page}`);
       
       
       return data
