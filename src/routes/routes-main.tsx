905afa9ccffc4d5685c51ee0063a972d57c36ed6
capellinx <capellaaa7@gmail.com>
2024-12-08 13:33:11 -0300

    feat: create new route to product page

--- src/routes/routes-main.tsx
+++ src/routes/routes-main.tsx
@@ -3,6 +3,7 @@ import {NotFound} from '../app/404';
 import {CostumersPage} from '../app/costumers';
 import {HomePage} from '../app/home';
 import {LoginPage} from '../app/login';
+import ConstructionPage from '../app/page-building';
 import {RegisterPage} from '../app/register';
 import {AuthControllerAccess} from '../utils/auth-control-access.tsx';
 
@@ -16,6 +17,7 @@ export const RoutesMain = () => {
       <Route element={<AuthControllerAccess/>}>
         <Route path="/home" element={<HomePage/>}/>
         <Route path="/costumers" element={<CostumersPage/>}/>
+        <Route path="/products" element={<ConstructionPage/>}/>
       </Route>
       
       <Route path="*" element={<NotFound />} />
