b84bed5fef7e02e0be6322835afe22b614328326
capellinx <capellaaa7@gmail.com>
2024-12-08 13:11:37 -0300

    feat: create logic to paginanate costumers

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,5 +1,4 @@
-import {useState} from 'react';
-import {Costumer} from '../../@types/costumer';
+import {ChangeEvent, useState} from 'react';
 import {CreateCostumerForm} from '../../components/forms/create-cosutmer-form';
 import {Header} from '../../components/header';
 import {
@@ -10,8 +9,25 @@ import {useGetCostumer} from './hooks/use-get-costumer.ts';
 
 export function HomePage() {
   const [isOpen, setIsOpen] = useState(false);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [limitPage, setLimitPage] = useState<number>(12);
   
-  const { costumers } = useGetCostumer()
+  const { costumers } = useGetCostumer({
+    page: currentPage,
+    limit: limitPage,
+  })
+  
+  function goToPreviousPage() {
+    setCurrentPage((prev) => Math.max(prev - 1, 1));
+  };
+  
+  function goToNextPage() {
+    setCurrentPage((prev) => Math.min(prev + 1, costumers?.meta?.totalPages!));
+  };
+  
+  function handleSelectLimitPage(event: ChangeEvent<HTMLSelectElement>) {
+    setLimitPage(Number(event.target.value))
+  }
   
   return (
     <main>
@@ -20,18 +36,28 @@ export function HomePage() {
         <header
           className="flex flex-col gap-3 pb-3 justify-between items-center sm:flex-row sm:gap-0 sm:p-0">
           <span>
-            <strong>{costumers.length}</strong> clientes econtrados:
+            <strong>{costumers?.items?.length}</strong> clientes econtrados:
           </span>
           <div className="flex items-center justify-center gap-3">
             <label>Clientes por página:</label>
-            <select id="cars" name="cars" className="border-2 border-gray rounded-md bg-white w-10">
-              {costumers.map((costumer: Costumer.List) => (
-                <option value="volvo" key={costumer.id}>{costumer.id}</option>
-              ))}
+            <select
+              onChange={handleSelectLimitPage}
+              defaultValue={limitPage}
+              className="border-2 border-gray rounded-md bg-white w-10">
+                <option value="4">4</option>
+                <option value="8">8</option>
+                <option value="12">12</option>
+                <option value="16">16</option>
             </select>
           </div>
         </header>
-        <CostumersList costumers={costumers}/>
+        <CostumersList
+          costumers={costumers.items}
+          meta={costumers.meta}
+          goToNextPage={goToNextPage}
+          goToPreviousPage={goToPreviousPage}
+          currentPage={currentPage}
+        />
         <button
           onClick={() => setIsOpen(!isOpen)}
           className="bg-transparent w-full border-2 text-orange-500 border-orange-500 rounded-md mt-5 p-2 hover:bg-orange-700 hover:border-orange-700 hover:text-white transform transition duration-300">
@@ -45,7 +71,9 @@ export function HomePage() {
           setIsOpen={setIsOpen}
           title="Cadastrar cliente:"
         >
-          <CreateCostumerForm setIsOpen={setIsOpen}/>
+          <CreateCostumerForm
+            setIsOpen={setIsOpen}
+          />
         </ModalCostumers>
       )}
     </main>
