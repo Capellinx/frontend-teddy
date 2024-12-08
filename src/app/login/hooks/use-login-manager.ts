1cc245f42845f4eb770d9e0540cb3dcfae128cc7
capellinx <capellaaa7@gmail.com>
2024-12-08 01:50:38 -0300

    feat: create hook to login

---
+++ src/app/login/hooks/use-login-manager.ts
@@ -0,0 +1,54 @@
+import {useMutation} from '@tanstack/react-query';
+import toast from 'react-hot-toast';
+import {z} from 'zod';
+import {useAuthContext} from '../../../hooks/use-auth.ts';
+// import {useNavigate} from 'react-router-dom';
+import {api} from '../../../service/api.ts';
+import {schemaLoginManager} from '../schema/schema-login-manager.ts';
+
+
+export const useLoginManager = () => {
+  // const navigate = useNavigate();
+  const {saveInformationToLocalStorage} = useAuthContext()
+  
+  const mutate = useMutation({
+    mutationKey: ['login'],
+    onSuccess: () => {
+      // navigate("/")
+    },
+    mutationFn: async ({username, password}: z.infer<typeof schemaLoginManager>) => {
+      await handleLoginManager({username, password})
+    },
+  })
+  
+  const handleLoginManager = async ({username, password}: z.infer<typeof schemaLoginManager>) => {
+    try {
+      const { data } = await api.post(`/admin/login`, {
+        username,
+        password
+      })
+      
+      saveInformationToLocalStorage(data)
+      
+      return data
+    } catch (error) {
+      throw error;
+    }
+  };
+  
+  const handleLogin = async ({username, password}: z.infer<typeof schemaLoginManager>) => {
+    await toast.promise(
+      mutate.mutateAsync({username, password}),
+      {
+        loading: 'Acessando...',
+        success: 'Acessou com sucesso!',
+        error: 'Erro ao acessar.',
+      },
+    );
+  };
+  
+  
+  return {
+    handleLogin
+  }
+}
