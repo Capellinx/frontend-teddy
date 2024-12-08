10ba87a08dc96f6c177e718fc4bf41551b512425
capellinx <capellaaa7@gmail.com>
2024-12-08 08:40:58 -0300

    feat: create hook to register costumer

---
+++ src/components/forms/create-cosutmer-form/hooks/use-register-costumer.ts
@@ -0,0 +1,43 @@
+import {useMutation} from '@tanstack/react-query';
+import toast from 'react-hot-toast';
+import {Costumer} from '../../../../@types/costumer';
+import {api} from '../../../../service/api.ts';
+
+
+export const useRegisterCostumer = () => {
+  
+  const mutate = useMutation({
+    mutationKey: ['register'],
+    mutationFn: async ({name, salary, company}: Costumer.Create) => {
+      await handleRegisterCostumer({name, salary, company})
+    },
+  })
+  
+  const handleRegisterCostumer = async ({name, salary, company}: Costumer.Create) => {
+    try {
+      return await api.post(`/costumer`, {
+        name,
+        salary, company
+      })
+      
+    } catch (error) {
+      throw error;
+    }
+  };
+  
+  const handleRegister = async ({name, salary, company}: Costumer.Create) => {
+    await toast.promise(
+      mutate.mutateAsync({name, salary, company}),
+      {
+        loading: 'Registrando...',
+        success: 'Registrado com sucesso!',
+        error: 'Erro ao registrar.',
+      },
+    );
+  };
+  
+  
+  return {
+    handleRegister
+  }
+}
