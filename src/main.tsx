a5f810616498a6c488acc7226a0bcfe02821e344
capellinx <capellaaa7@gmail.com>
2024-12-08 00:50:08 -0300

    feat: instance react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,26 @@
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
+import {Toaster} from 'react-hot-toast';
 import {BrowserRouter} from 'react-router-dom'
 import {App} from './App.tsx'
 import './index.css'
 import { AuthProvider } from './contexts/auth-context.tsx'
 import {CostumerProvider} from './contexts/costumer-context.tsx';
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <CostumerProvider>
-          <App/>
-        </CostumerProvider>
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <CostumerProvider>
+            <App/>
+          </CostumerProvider>
+        </AuthProvider>
+        <Toaster/>
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
 )
