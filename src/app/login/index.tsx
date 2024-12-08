e9d94492a12324a0208c49cb1ee18cfb8793e8b2
capellinx <capellaaa7@gmail.com>
2024-12-08 01:51:24 -0300

    refactor: implements react hook form

--- src/app/login/index.tsx
+++ src/app/login/index.tsx
@@ -1,52 +1,56 @@
-import { ChangeEvent, FormEvent, InvalidEvent, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useAuthContext } from "../../hooks/use-auth.ts";
+import {zodResolver} from '@hookform/resolvers/zod';
+import {useForm} from 'react-hook-form';
+import {useNavigate} from 'react-router-dom';
+import {z} from 'zod';
+import {useLoginManager} from './hooks/use-login-manager.ts';
+import {schemaLoginManager} from './schema/schema-login-manager.ts';
 
 export function LoginPage() {
-  const [newName, setNewName] = useState("");
   
-  const { saveInformationToLocalStorage } = useAuthContext();
+  const {register, handleSubmit } = useForm<z.infer<typeof schemaLoginManager>>({
+    defaultValues: {
+      username: '',
+      password: '',
+    },
+    mode: 'onChange',
+    resolver: zodResolver(schemaLoginManager),
+  })
   
+  const { handleLogin } = useLoginManager()
   const navigate = useNavigate();
   
-  function handleSubmitName(event: FormEvent) {
-    event.preventDefault();
-    
-    saveInformationToLocalStorage(newName);
+  function onSubmit({username, password}: z.infer<typeof schemaLoginManager>) {
+    handleLogin({username, password});
   }
   
-  function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
-    event.target.setCustomValidity("");
-    
-    setNewName(event.target.value);
-  }
-  
-  function handleNameInvalid(event: InvalidEvent<HTMLInputElement>) {
-    event.target.setCustomValidity("Esse campo é obrigatório");
-  }
   
   function leaveUserToRegisterPage() {
-    navigate("/register");
+    navigate('/register');
   }
   
   return (
     <main className="w-full h-screen flex flex-col justify-center items-center">
       <form
-        onSubmit={handleSubmitName}
+        onSubmit={handleSubmit(onSubmit)}
         className="w-full max-w-sm flex flex-col items-center justify-center gap-6 p-6 bg-white shadow-lg rounded-lg"
       >
         <h1 className="text-3xl font-inter font-light text-center">
           Olá, seja bem-vindo
         </h1>
         <input
-          required
-          onChange={handleInputChange}
-          onInvalid={handleNameInvalid}
-          placeholder="Digite seu nome:"
           type="text"
+          {...register("username")}
+          placeholder="Digite seu nome"
           className="w-full bg-transparent border-2 border-gray rounded-sm p-2 focus:outline-orange-500"
         />
-        <button className="w-full p-2 rounded-sm text-white bg-orange-500 focus:outline-black hover:bg-orange-700">
+        <input
+          type="text"
+          {...register("password")}
+          placeholder="Digite sua senha"
+          className="w-full bg-transparent border-2 border-gray rounded-sm p-2 focus:outline-orange-500"
+        />
+        <button
+          className="w-full p-2 rounded-sm text-white bg-orange-500 focus:outline-black hover:bg-orange-700">
           Entrar
         </button>
       </form>
