da07a5d6cf1e41980c7bb4c3096f3df75707ea5c
capellinx <capellaaa7@gmail.com>
2024-12-08 08:50:22 -0300

    feat: create new typing to list costumers

--- src/@types/costumer.d.ts
+++ src/@types/costumer.d.ts
@@ -14,5 +14,15 @@ export namespace Costumer {
     salary?: string;
     company?: string;
   }
+  
+  export type List = {
+    id: string,
+    name: string,
+    salary: number,
+    company: number,
+    is_selected: boolean,
+    created_at: Date,
+    updated_at: Date,
+  }
 }
 
